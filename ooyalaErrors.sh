b62b5a27a4371993b25942c827bbccc74df4deda
jacobben85 <jacobben85@gmail.com>
2014-11-19 15:44:02 -0500

    Ooyala ingestion log - add option to pull the Failed video ids

--- ooyalaErrors.sh
+++ ooyalaErrors.sh
@@ -1,7 +1,7 @@
-CURRENTDATE=` date +"%Y-%m-%d"`
+CURRENTDATE=` date +"%Y-%m-%d" -d "-1 day"`
 CURRENTDATE=$CURRENTDATE
 
-STRINGMATCH = 'Error'
+STRINGMATCH= 'Error'
 
 echo "Looking up errors in Ooyala logs for :"
 echo $CURRENTDATE
@@ -10,6 +10,9 @@ echo "\n\n"
 wget http://u1819.uolsite.univision.com/applogs/mylog_$CURRENTDATE
 
 grep -C 3 'Error' mylog_$CURRENTDATE
+
+echo "\n\nFailed ingestions for :"
+grep -C 3 'Error' mylog_$CURRENTDATE | grep "Start send request GET: /v2/assets/.* to Ooyala api" | grep -o '[0-9]\{5,7\}'
 
 echo "\n\nNumber of error reports in logs :"
 grep -c 'Error' mylog_$CURRENTDATE

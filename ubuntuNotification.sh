944f8862db906254e4daa2e992be68936d506a1c
jacobben85 <jacobben85@gmail.com>
2014-12-11 16:39:17 -0500

    Adding icon and beep to notification
    Adding icon and beep to notification
    there are dependencies that are yet to be taken care of

--- ubuntuNotification.sh
+++ ubuntuNotification.sh
@@ -1,2 +1,20 @@
-notify-send "Ubuntu Notification"
-paplay /usr/share/sounds/ubuntu/stereo/bell.ogg
+#!/bin/bash
+
+# notify-send "Notification" "Testing messages" -i $(pwd)/logo.png -t 5000
+# paplay $(pwd)/beep.ogg
+
+#
+# #!/bin/sh
+# touch $HOME/.dbus/Xdbus
+# chmod 600 $HOME/.dbus/Xdbus
+# env | grep DBUS_SESSION_BUS_ADDRESS > $HOME/.dbus/Xdbus
+# echo 'export DBUS_SESSION_BUS_ADDRESS' >> $HOME/.dbus/Xdbus
+# exit 0
+#
+
+if [ -r ~/.dbus/Xdbus ]; then
+  . ~/.dbus/Xdbus
+fi
+
+notify-send "Time Check" "$(date)" -i ~/usefulScripts/logo.png -t 5000
+paplay ~/usefulScripts/beep.ogg

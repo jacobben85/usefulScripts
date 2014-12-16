5090bfd8e0a921ae84d88c9f661f950b84d39add
jacobben85 <jacobben85@gmail.com>
2014-12-16 15:25:35 -0500

    List errors when detected
    List errors only when detected, add the notifier for ubuntu

--- ooyalaErrors.sh
+++ ooyalaErrors.sh
@@ -1,3 +1,7 @@
+if [ -r ~/.dbus/Xdbus ]; then
+  . ~/.dbus/Xdbus
+fi
+
 CURRENTDATE=` date +"%Y-%m-%d" -d "-1 hour"`
 CURRENTDATE=$CURRENTDATE
 
@@ -5,28 +9,34 @@ CURRENTHOUR=` date +"%m-%d-%Y @ %H" -d "-1 hour"`
 CURRENTHOUR=$CURRENTHOUR
 
 echo "Looking up errors in Ooyala logs for :"
-echo $CURRENTDATE
-
-echo "Hour :"
 echo $CURRENTHOUR
 
-wget http://u1819.uolsite.univision.com/applogs/mylog_$CURRENTDATE
+wget -q http://u1819.uolsite.univision.com/applogs/mylog_$CURRENTDATE
 
 echo ""
 echo "Total number of error reports in logs :"
 grep -c 'Error' mylog_$CURRENTDATE
 
-echo ""
-echo "Number of errors in last hour :"
-grep -c "$CURRENTHOUR:.*:.* Error" mylog_$CURRENTDATE
+ERRORCOUNT=`grep -c "$CURRENTHOUR:.*:.* Error" mylog_$CURRENTDATE`
 
-echo ""
-echo "Errors in last hour :"
-grep -C 3 'Error' mylog_$CURRENTDATE | grep "$CURRENTHOUR:.*:.*Start send request GET: /v2/assets/.* to Ooyala api" | grep -o '[0-9]\{5,7\}'
+notify-send "Ooyala log checker" "Last hour errors : $ERRORCOUNT" -i ~/usefulScripts/logo.png -t 5000
 
-echo ""
-echo "Details from Log :"
-grep -C 3 "$CURRENTHOUR:.*:.* Error" mylog_$CURRENTDATE
-echo ""
+if [ $ERRORCOUNT = "0" ]; then
+	echo ""
+	echo "No Errors in last hour"
+else
+	echo ""
+	echo "Number of errors in last hour :"
+	echo $ERRORCOUNT
+
+	echo ""
+	echo "Errors in last hour :"
+	grep -C 3 'Error' mylog_$CURRENTDATE | grep "$CURRENTHOUR:.*:.*Start send request GET: /v2/assets/.* to Ooyala api" | grep -o '[0-9]\{5,7\}'
+
+	echo ""
+	echo "Details from Log :"
+	grep -C 3 "$CURRENTHOUR:.*:.* Error" mylog_$CURRENTDATE
+	echo ""
+fi
 
 rm mylog_$CURRENTDATE

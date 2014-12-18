487af9f59edce485f09f8439e70f33083bb982e9
jacobben85 <jacobben85@gmail.com>
2014-12-18 12:09:17 -0500

    Few more scripts
    1. Virtual Box host SSH connector
    2. Connect the VM Guest to Vagrant
    3. django installer
    4. mpsyt

---
+++ django.sh
@@ -0,0 +1,10 @@
+sudo apt-get install apache2
+sudo apt-get install apache2.2-common
+sudo apt-get install apache2-mpm-prefork 
+sudo apt-get install apache2-utils 
+sudo apt-get install libexpat1 
+sudo apt-get install ssl-cert
+sudo apt-get install libapache2-mod-wsgi
+sudo service apache2 restart
+sudo apt-get install python-imaging python-pythonmagick python-markdown python-textile python-docutils
+sudo apt-get install python-django

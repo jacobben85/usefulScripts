923cae6d78ed736d079cd57f2d5046a7714648e8
jacobben85 <jacobben85@gmail.com>
2015-03-06 17:03:00 -0500

    Adding request hander

--- node/requestHandlers.js
+++ node/requestHandlers.js
@@ -7,8 +7,9 @@
 
 /* global exports, require */
 var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function start(response) {
+function start_old(response) {
     exec("ls -lah", function (error, stdout, stderr) { 
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write(stdout);
@@ -19,9 +20,32 @@ function start(response) {
     });
     console.log("Request handler 'start' was called.");
 }
-function upload(response) {
+function upload_old(response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Upload Page");
+    response.end();
+    console.log("Request handler 'upload' was called.");
+}
+function start(response, postData) {
+    var body = '<html>'+
+            '<head>'+
+            '<meta http-equiv="Content-Type" content="text/html; '+ 
+            'charset=UTF-8" />'+
+            '</head>'+
+            '<body>'+
+            '<form action="/upload" method="post">'+
+            '<textarea name="text" rows="20" cols="60"></textarea>'+ 
+            '<input type="submit" value="Submit text" />'+ 
+            '</form>'+
+            '</body>'+
+            '</html>';
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+}
+function upload(response, postData) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(querystring.parse(postData).text);
     response.end();
     console.log("Request handler 'upload' was called.");
 }
